dd347afa450ad2f002bfe4aeb83cf6f79cd4877d
Logan <73146572+Logan20063@users.noreply.github.com>
2021-02-07 15:38:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,87 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomColor() {
+  number = random(1, 6)
+  if(number == 1) {
+    //red
+    return "#FF0000"
+  }
+  if(number == 2) {
+    //orange
+    return "#FF9900"
+  }
+  if(number == 3) {
+    //yellow
+    return "#FFFF00"
+  }
+  if(number == 4) {
+    //green
+    return "#00FF00"
+  }
+  if(number == 5) {
+    //blue
+    return "#0000FF"
+  }
+  if(number == 6) {
+    //purple
+    return "#9900FF"
+  }
+}
+
+function embedZeroFields(color, title) {
+  let embed = new Discord.MessageEmbed()
+    .setColor(color)
+    .setTitle(title)
+  return embed
+}
+
+function embedOneField(color, title, name, value) {
+  let embed = new Discord.MessageEmbed()
+    .setColor(color)
+    .setTitle(title)
+    .addField(name, value)
+  return embed
+}
+
+function embedTwoFields(color, title, field1Name, feild1Value, field2Name, field2Value) {
+  let embed = new Discord.MessageEmbed()
+    .setColor(color)
+    .setTitle(title)
+    .addFields(
+      { name: field1Name, value: feild1Value},
+      { name: field2Name, value: field2Value}
+    )
+  return embed
+}
+
+function embedThreeFields(color, title, field1Name, feild1Value, field2Name, field2Value, field3Name, field3Value) {
+  let embed = new Discord.MessageEmbed()
+    .setColor(color)
+    .setTitle(title)
+    .addFields(
+      { name: field1Name, value: feild1Value},
+      { name: field2Name, value: field2Value},
+      { name: field3Name, value: field3Value}
+    )
+  return embed
+}
+
+function checkAdmin(message) {
+  if (message.member.hasPermission('ADMINISTRATOR')) {
+    console.log("admin")
+    return 1
+  } else {
+    message.channel.send(embedZeroFields(randomColor(), ":x:You Need To Have Admin:x:"))
+  }
+}
+
 let picked = 0
 let numbers = {}
 let leaderboard = {}
+let embed
 
+const Discord = require('discord.js');
 const { Client } = require("discord.js");
 const keepAlive = require('./server.js');
  
@@ -18,6 +95,19 @@ const client = new Client({
 keepAlive();
 
 client.on('message', message => {
+  if(message.content === "reset") {
+    if(checkAdmin(message)) {
+      numbers[message.guild.id + "prefix"] = "^"
+      message.channel.send(embedZeroFields(randomColor(), "Prefix reset to ^"))
+    }
+  }
+  if(message.content === "admin") {
+    if (checkAdmin(message)) {
+      message.reply(" has admin")
+    } else {
+      message.reply(" does not have admin")
+    }
+  }
   if(isNaN(numbers[message.guild.id +"min"])) {
     numbers[message.guild.id +"min"] = 1
   }
@@ -27,8 +117,9 @@ client.on('message', message => {
   if(isNaN(numbers[message.guild.id +"total"])) {
     numbers[message.guild.id +"total"] = 4
   }
-  if(isNaN(numbers[message.guild.id +"prefix"])) {
+  if(numbers[message.guild.id +"prefix"] == undefined) {
     numbers[message.guild.id +"prefix"] = "^"
+    console.log("1")
   }
   
   if (message.author.bot || message.content.charAt(0) != numbers[message.guild.id + "prefix"]) {
@@ -39,8 +130,15 @@ client.on('message', message => {
     message.content = message.content.substring(1)
   }
   message.content = message.content.split(" ")
+  
+  //help
   if (message.content[0] === 'help') {
-    message.reply('https://docs.google.com/document/d/1dWMOoGXSzHVW7o-H8HEa7Lss3WfZ5PDTdRTqbQWjxio/edit?usp=sharing')
+    embed = new Discord.MessageEmbed()
+      .setColor(randomColor())
+      .setTitle("HELP")
+      .setURL("https://docs.google.com/document/d/1dWMOoGXSzHVW7o-H8HEa7Lss3WfZ5PDTdRTqbQWjxio/edit")
+    
+    message.channel.send(embed)
   }
   
   //start
@@ -57,7 +155,7 @@ client.on('message', message => {
     
     picked = random(numbers[message.author.id + "min"], numbers[message.author.id + "max"])
 
-	 message.channel.send('picked');
+	 message.channel.send(embedTwoFields(randomColor(), "Picked", "parameters", numbers[message.author.id + "min"] + "-" + numbers[message.author.id + "max"], "guesses", numbers[message.author.id + "total"]));
 
    numbers[message.author.id] = picked
    numbers[message.author.id + "guesses"] = numbers[message.author.id + "total"]
@@ -65,7 +163,7 @@ client.on('message', message => {
 
   //higher/lower
   if (message.content[0] < numbers[message.author.id] && numbers[message.author.id + "guesses"] > 0) {
-   message.reply(':arrow_up:higher:arrow_up:');
+   message.channel.send(embedZeroFields(randomColor(), ':arrow_up:higher:arrow_up:'));
    numbers[message.author.id + "guesses"] -= 1
    if (numbers[message.author.id + "guesses"] == 0) {
       message.reply('game over')
@@ -73,7 +171,7 @@ client.on('message', message => {
     }
   }
   if (message.content[0] > numbers[message.author.id] && numbers[message.author.id + "guesses"] > 0) {
-    message.reply(':arrow_down:lower:arrow_down:')
+    message.channel.send(embedZeroFields(randomColor(), ':arrow_down:lower:arrow_down:'))
     numbers[message.author.id + "guesses"] -= 1
     if (numbers[message.author.id + "guesses"] == 0) {
       message.reply('game over')
@@ -81,30 +179,37 @@ client.on('message', message => {
     }
   }
   if (message.content[0] == numbers[message.author.id] && numbers[message.author.id + "guesses"] > 0) {
-    if(isNaN(leaderboard[message.author.username])) {
-      leaderboard[message.author.username] = 0
+    if(isNaN(leaderboard[message.author.id])) {
+      leaderboard[message.author.id] = 0
     }
+
+    numbers[message.author.id + "guesses"] = 0
 
     score = (numbers[message.author.id + "max"] - numbers[message.author.id + "min"] + 1) - Math.pow(2, numbers[message.author.id + "total"])
     console.log(score)
 
-    if(score > 1){
-      leaderboard[message.author.username] += score
-      message.reply(':white_check_mark:congradulations you got ' + score + ' points:white_check_mark:')
-    } else {
-      leaderboard[message.author.username] += 1
-      message.reply(':white_check_mark:congradulations you got a point:white_check_mark:')
+    if(score < 1){
+      score = 1    
     }
+    leaderboard[message.author.id] += score
+    message.channel.send(embedOneField(randomColor(), ':white_check_mark:Congradulations you won:white_check_mark:', 'score', score)) 
   }
 
   //prefix
   if(message.content[0] === "prefix") {
-    if(message.content[1] == undefined) {
+    if(checkAdmin(message)) { 
+      oldPrefix = numbers[message.guild.id + "prefix"]
+      if(message.content[1] == undefined) {
       message.reply('you need an actual prefix')
-      return
+        return
+      }
+      if(message.content[1].length > 1) {
+        message.reply("prefixes can only be one character")
+        return
+      }
+      numbers[message.guild.id + "prefix"] = message.content[1]
+      message.channel.send(embedTwoFields(randomColor(), "Prefix Changed", "Old Prefix", oldPrefix, "New Prefix", numbers[message.guild.id + "prefix"]))
     }
-    numbers[message.guild.id + "prefix"] = message.content[1]
-    message.channel.send('prefix = ' + numbers[message.guild.id + "prefix"])
   }
 
   //minmax
@@ -118,7 +223,7 @@ client.on('message', message => {
     }
     numbers[message.author.id + "min"] = message.content[1]
     numbers[message.author.id + "max"] = message.content[2]
-    message.channel.send('minimum = ' + numbers[message.author.id + "min"] + "\n maximum = " + numbers[message.author.id + "max"])
+    message.channel.send(embedTwoFields(randomColor(), "MinMax changed", "New Minimum", numbers[message.author.id + "min"], "New Maximum", numbers[message.author.id + "max"]))
   }
 
   //guesses amount
@@ -129,17 +234,20 @@ client.on('message', message => {
       return
     }
     numbers[message.author.id + "total"] = message.content[1]
-    message.channel.send('"guesses" = ' + numbers[message.author.id + "total"])
+    message.channel.send(embedOneField(randomColor(), "Guesses Changed", "New Guesses", numbers[message.author.id + "total"]))
   }
 
-  //"guesses" left
+  //guesses left
   if(message.content[0] === "guessesleft") {
-    message.reply("has " + numbers[message.author.id + "guesses"] + " guesses left")
+    message.channel.send(embedOneField(randomColor(), "Guesses Left", "Amount", numbers[message.author.id + "guesses"]))
   } 
 
   //score
   if(message.content[0] === "score") {
-    message.reply(leaderboard[message.author.username])
+   if(isNaN(leaderboard[message.author.id])){
+     leaderboard[message.author.id] = 0
+   }
+    message.channel.send(embedOneField(randomColor(), "Score", message.author.username, leaderboard[message.author.id]))
   }
 
   //settings
@@ -154,7 +262,7 @@ client.on('message', message => {
      numbers[message.author.id + "total"] = numbers[message.guild.id + "total"]
     }
     
-    message.reply("minimum = " + numbers[message.author.id + "min"] + "\nmaximum = " + numbers[message.author.id + "max"] + "\nguesses = " + numbers[message.author.id + "total"])
+    message.channel.send(embedThreeFields(randomColor(), "Settings", "Minimum", numbers[message.author.id + "min"], "Maximum", numbers[message.author.id + "max"], "Guesses", numbers[message.author.id + "total"]))
   }
 
   if (message.content[0] === "default") {
@@ -163,18 +271,19 @@ client.on('message', message => {
     message.content[3] = parseInt(message.content[3])
 
     if (isNaN(message.content[1]) || isNaN(message.content[2]) || isNaN(message.content[3])) {
-      message.reply('default minimum = ' + numbers[message.guild.id + "min"] + '\ndefault maximum = ' + numbers[message.guild.id + "max"] + "\ndefault guesses = " + numbers[message.guild.id + "total"])
+      message.channel.send(embedThreeFields(randomColor(),"Default Settings", "Minimum", numbers[message.guild.id + "min"], "Maximum", numbers[message.guild.id + "max"], "Guesses", numbers[message.guild.id + "total"]))
       return
     }
-
-    numbers[message.guild.id + "min"] = message.content[1] 
-    numbers[message.guild.id + "max"] = message.content[2]
-    numbers[message.guild.id + "total"] = message.content[3]
-    message.reply(":white_check_mark:default settings have been changed:white_check_mark:")
+    if(checkAdmin(message)){
+      numbers[message.guild.id + "min"] = message.content[1] 
+      numbers[message.guild.id + "max"] = message.content[2]
+      numbers[message.guild.id + "total"] = message.content[3]
+      message.channel.send(embedThreeFields(randomColor(),":white_check_mark:default settings have been changed:white_check_mark:", "Minimum", numbers[message.guild.id + "min"], "Maximum", numbers[message.guild.id + "max"], "Guesses", numbers[message.guild.id + "total"]))
+    }
   }
 
   if(message.content[0] === "ping") {
-    message.reply('pong:ping_pong:')
+    message.channel.send(embedZeroFields('#FF0000', 'pong:ping_pong:'))
   }
 
   if(message.content[0] === "playing") {
